e9196dbc7e24046d1565f2a9b7b1c98f736224d8
andres <afcanop@gmail.com>
2022-10-31 15:31:59 -0500

    feat: cambios en la DIRS

--- testmicroServicio/settings.py
+++ testmicroServicio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'testmicroServicio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'public']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
